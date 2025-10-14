acd2d5eb8bf5dc601f830d3443004633d2ae5fb4
cse24-038 <cse24-038@thuto.bac.ac.bw>
2025-10-14 19:56:42 +0200

    Update public abstract class Account {.java

--- public abstract class Account {.java
+++ public abstract class Account {.java
@@ -1,9 +1,7 @@
 public abstract class Account {
-    // Attributes
     private String accountNumber;
     private double accountBalance;
     private String accountBranch;
-
 
     public Account(String accountNumber, String accountBranch, double initialBalance) {
         this.accountNumber = accountNumber;
@@ -11,30 +9,31 @@ public abstract class Account {
         this.accountBalance = initialBalance;
     }
 
-
     public void deposit(double amount) {
         if (amount > 0) {
             this.accountBalance += amount;
-            System.out.printf("Deposited %.2f into account %s. New balance: %.2f%n", amount, this.accountNumber, this.accountBalance);
+            System.out.printf("Deposited %.2f into account %s. New balance: %.2f%n",
+                    amount, this.accountNumber, this.accountBalance);
         } else {
             System.out.println("Deposit amount must be positive.");
         }
     }
 
-    
-    public abstract void withdraw(double amount); // Abstract as per the diagram's design
+    public abstract void withdraw(double amount);
 
-    
     public double getBalance() {
         return accountBalance;
     }
 
     public String getAccountNumber() {
         return accountNumber;
+    }
 
+    public String getAccountBranch() {
+        return accountBranch;
+    }
 
     protected void setBalance(double newBalance) {
         this.accountBalance = newBalance;
     }
 }
-

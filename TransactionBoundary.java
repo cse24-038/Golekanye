fa935c22519cbc28fd60c1aa4293e4ea65ec7b2c
cse24-038 <cse24-038@thuto.bac.ac.bw>
2025-11-26 10:36:55 +0200

    Delete TransactionBoundary.java

--- TransactionBoundary.java
+++
@@ -1,9 +0,0 @@
-package com.bank.boundary;
-
-import java.math.BigDecimal;
-
-public interface TransactionBoundary {
-    BigDecimal getDepositAmount() throws NumberFormatException;
-    BigDecimal getWithdrawalAmount() throws NumberFormatException;
-    void displayTransactionResult(String message);
-}
